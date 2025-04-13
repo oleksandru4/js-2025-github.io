ff451b17428788aaa565e5ca17ef4dced24e5aa8
oleksandru4 <rzsz030@gmail.com>
2025-04-14 01:29:26 +0300

    Lessons-18, add task-6

--- HomeAssignments/Lessons-18/task_06/script.js
+++ HomeAssignments/Lessons-18/task_06/script.js
@@ -1,7 +1,21 @@
-/*The user specifies the number of grades and clicks the “get table” button.
- As a result, a table with input is formed, where the user enters the grades.
+/* 6: The user specifies the number of grades and clicks the “get table” button.
+ As a result, a table with input is formed, where the user enters the grades. 
  After that, he clicks the “get sum” button and finds the average value.
 */
+
+function changeRating() {
+    let containerTable = document.querySelector('.render-table')
+    let span = containerTable.querySelector('.average-value')
+    let ratingList = containerTable.querySelectorAll('input')
+    let sumValue = 0
+
+    ratingList.forEach((el) => {
+        if (el.valueAsNumber)
+            sumValue += el.valueAsNumber
+    })
+    span.innerText = sumValue / ratingList.length
+}
+
 
 function getGradeNumber() {
     let gradeNumberValue = document.querySelector('#gradeNumber').valueAsNumber
@@ -10,63 +24,81 @@ function getGradeNumber() {
     return 0
 }
 
-function renderTable() {
-    let rowNumber = getGradeNumber()
-    if (! rowNumber) return
+
+function renderTableContainer() {
+    // v.1: через createElement 😱🤯
+
+    // let tableEl = document.createElement('table')
+    // tableEl.className="table"
+
+    // let theadEl = document.createElement('thead')
+    // let trEl = document.createElement('tr')
+    // let th1El = document.createElement('th')
+    // th1El.innerText = '#'
+    // let th2El = document.createElement('th')
+    // let span1El = document.createElement('span')
+    // span1El.innerText = 'Середня оцінка:'
+    // let span2El = document.createElement('span')
+    // span2El.className = 'average-value'
+
+    // tableEl.append(theadEl)
+    // theadEl.append(trEl)
+    // trEl.append(th1El)
+    // trEl.append(th2El)
+    // th2El.append(span1El)
+    // th2El.append(span2El)
+    // document.querySelector('.render-table').append(tableEl)
+
+    // v.2: через innerHTML (і життя стає простішим 🙂)
 
     let renderHTML = `
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Середня оцінка: <span>sdas</span></th>
+                    <th scope="col">Середня оцінка : &nbsp &nbsp <span class="average-value"></span></th>
                 </tr>
             </thead>
-            <tbody>        
-    `
-
-    for (let i = 0; i < rowNumber; i++) {
-        
-
-    }
-
-    renderHTML += `
-            </tbody>
+            <tbody></tbody>
         </table>
     `
+    document.querySelector('.render-table').innerHTML = renderHTML
+
 
 }
 
+function renderTable() {
+    let rowNumber = getGradeNumber()
+    let containerTable = document.querySelector('.render-table')
+    if (! rowNumber) {
+        containerTable.innerHTML = ''
+        return
+    }
 
-window.onload = function() {
-    document.querySelector('.get-table').onClick = renderTable
-}
+    renderTableContainer()
+    let renderHTML = ''
 
-
-/*function renderTable() {
-    let renderHTML = `
-        <table class="table">
-            <tbody>
-    `
-
-    for (var i = 0; i < 3; i++) {
+    for (let i = 0; i < rowNumber; i++) {
         renderHTML += `
             <tr>
-              <td>${Math.floor(Math.random() * 1000)}</td>
-              <td>${Math.floor(Math.random() * 1000)}</td>
-              <td>${Math.floor(Math.random() * 1000)}</td>
-              <td>${Math.floor(Math.random() * 1000)}</td>
+                <th scope="row">${i+1}</th>
+                <td >
+                    <div class="col-auto">
+                        <input type="number" class="form-control">
+                    </div>
+                </td>
             </tr>
         `
     }
-    
-    renderHTML += `
-            </tbody>
-        </table>
-    `
-    document.querySelector('.divForTable').innerHTML = renderHTML
+
+    containerTable.querySelector('tbody').innerHTML = renderHTML
+    containerTable.querySelectorAll('input').forEach((el)=>{
+        el.onchange = changeRating
+    })
 }
 
+
+
 window.onload = function() {
-    renderTable()
-}*/
+    document.querySelector('.get-table').onclick = renderTable
+}
